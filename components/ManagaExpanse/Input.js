fb594bacf0adfe826de45d1d20a811e455fd3845
dw-dev <dimaswahyu.official@gmail.com>
2024-05-13 09:43:25 +0700

    addd git

---
+++ components/ManagaExpanse/Input.js
@@ -0,0 +1,10 @@
+import { View, TextInput , Text} from "react-native"
+
+function Input({label, textInputConfig}) {
+    return <View>
+        <Text>{label}</Text>
+        <TextInput  {...textInputConfig} />
+    </View>
+}
+
+export default Input
